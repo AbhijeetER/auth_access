e7df1971b5776afe7547d95131ee528f4f044fed
AbhijeetER <206299249+AbhijeetER@users.noreply.github.com>
2026-04-19 00:36:23 +0530

    made sme updates

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,3 +3,5 @@ export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "http://localhost:3000"
 })
+
+//the lines above are as per docs and below we define and import the  type of auth and session and cache we want by going through different set of instruction o better-auth https://better-auth.com/docs/installation
