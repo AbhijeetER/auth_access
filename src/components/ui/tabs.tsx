07a162d7da29c9670f3ef5d28b7448c7daf22fd9
AbhijeetER <206299249+AbhijeetER@users.noreply.github.com>
2026-04-19 01:25:42 +0530

    tabs component corrected

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import * as TabsPrimitive from "@radix-ui/react-tabs";
-
+//new imports may have error after jan 2026
 
 import { cn } from "@/lib/utils"
 
